5cc02bd39b4e825256ba3e74b6af61ed1fa388d1
Iqra Suleman <iqrasuleman073@gmail.com>
2026-08-16 20:22:17 +0500

    Added exercise3 for topic 2 variables

---
+++ 2_variables/exercise3.py
@@ -0,0 +1,12 @@
+'''0Name= "Iqra" #invalid variable name
+!age = 20 #invalid variable name
+<City = "Arifwala" #invalid variable name'''
+
+
+name = "Iqra Suleman"
+age = 20
+city = "Arifwala"
+
+print ("My name is ", name)
+print ("I am ", age, " years old")
+print ("I live in ", city)
