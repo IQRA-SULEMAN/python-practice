4eda4f35e486c1fa4cc13a32f7ac1599f83dad9b
Iqra Suleman <iqrasuleman073@gmail.com>
2026-08-16 20:21:21 +0500

    Added exercise1 for topic 2 variables

---
+++ 2_variables/exercise1.py
@@ -0,0 +1,10 @@
+name = "Iqra Suleman"
+age = 20
+city = "Arifwala"
+favorite_color = "Blue"
+student_status = "Retired!"
+print("My name is ", name)
+print("I am ", age, " years old")
+print("I live in ", city)
+print("My favorite color is ", favorite_color)
+print("I am currently ", student_status)                
